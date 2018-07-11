208135def2d569093148108c56315e39211c5427
zhupengfei <1178227913@qq.com>
2018-07-11 18:32:00 +0800

    update player

--- assets/test/Test.js
+++ assets/test/Test.js
@@ -25,12 +25,12 @@ cc.Class({
         this._initMsg();
 
         cc.director.getPhysicsManager().enabled = true;
-        // cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
-        // cc.PhysicsManager.DrawBits.e_pairBit |
-        // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
-        // cc.PhysicsManager.DrawBits.e_jointBit |
-        // cc.PhysicsManager.DrawBits.e_shapeBit
-        // ;
+        cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
+        cc.PhysicsManager.DrawBits.e_pairBit |
+        cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+        cc.PhysicsManager.DrawBits.e_jointBit |
+        cc.PhysicsManager.DrawBits.e_shapeBit
+        ;
         cc.director.getPhysicsManager().gravity = cc.v2(0, -320);
 
         // this.touchLayer.on('touchstart', function (event) {
@@ -39,12 +39,9 @@ cc.Class({
         //         // this.spPlayer.node.scaleY = -this.spPlayer.node.scaleY;
         //     });
         // }.bind(this));
-        // console.log(cc.find('Canvas/stageLayer/Block0'));
-        // console.log(cc.find('Canvas/stageLayer/Block1'));
-        // Util.readJsonData('data/json/stageData/Stage1', this._loadStage);
-        // let self = this;
-        Util.loadJsonFile('data/json/stageData/Stage1', this._initBlock, this);
         
+        Util.loadJsonFile('data/json/stageData/Stage1', this._initBlock, this);
+        this.spPlayer.node.position = cc.p(430, 220);
  
     },
 
@@ -52,7 +49,10 @@ cc.Class({
 
     },
 
-    // update (dt) {},
+    update (dt) {
+
+    },
+
     _initBlock(results){
         this._stageData = results;
         console.log(this._stageData);
