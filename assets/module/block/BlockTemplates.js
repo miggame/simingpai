99e4fc8e09af8fa093e3252e4ec5ed832df1d4d4
zhupengfei <1178227913@qq.com>
2018-07-11 17:15:19 +0800

    update

---
+++ assets/module/block/BlockTemplates.js
@@ -0,0 +1,85 @@
+module.exports = {
+    templateList:[
+        {
+            id: 1,
+            textureName: 'Block2_55X50.png'
+        },
+        {
+            id: 2,
+            textureName: 'Block1_55X50.png'
+        },
+        {
+            id: 3,
+            textureName: 'Block1_110X50.png'
+        },
+        {
+            id: 4,
+            textureName: 'Block1_220X50.png'
+        },
+        {
+            id: 5,
+            textureName: '2Block1_55X56.png'
+        },
+        {
+            id: 6,
+            textureName: '2Block1_1_40X36.png'
+        },
+        {
+            id: 7,
+            textureName: '2Block2_56X50.png'
+        },
+        {
+            id: 8,
+            textureName: '3Block1_55X50.png'
+        },
+        {
+            id: 9,
+            textureName: '3Block1_40X36.png'
+        },
+        {
+            id: 10,
+            textureName: '3Block1_1_55X50.png'
+        },
+        {
+            id: 11,
+            textureName: '3Block1_2_55X50.png'
+        },
+        {
+            id: 12,
+            textureName: '4Block1_40X36.png'
+        },
+        {
+            id: 13,
+            textureName: '4Block1_55X50.png'
+        },
+        {
+            id: 14,
+            textureName: '5Block1_55X50.png'
+        },
+        {
+            id: 15,
+            textureName: '5Block1_40X36.png'
+        },
+        {
+            id: 16,
+            textureName: 'Block1_UPPillar.png'
+        },
+        {
+            id: 17,
+            textureName: 'Block1_DownPillar.png'
+        },
+        {
+            id: 18,
+            textureName: '3Block1_2_55X50.png'
+        },
+    ],
+
+    getBlockNameById(id){
+        console.log('id: ', id);
+        for (const item of this.templateList) {
+            if(id === item.id){
+                return item.textureName.split('.')[0];
+            }
+        }
+    }
+}
--- assets/test/Test.js
+++ assets/test/Test.js
@@ -1,11 +1,15 @@
 let Observer = require('Observer');
 let UIMgr = require('UIMgr');
+let Util = require('Util');
+let BlockTemmplates = require('BlockTemplates');
 cc.Class({
     extends: Observer,
 
     properties: {
         spPlayer: { displayName: 'spPlayerk', default: null, type: cc.Sprite },
         touchLayer: { displayName: 'touchLayer', default: null, type: cc.Node },
+        _stageData: null,
+        blockLayer: { displayName: 'blockLayer', default: null, type: cc.Node },
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -19,12 +23,29 @@ cc.Class({
 
     onLoad() {
         this._initMsg();
-        this.touchLayer.on('touchstart', function (event) {
-            // this.spPlayer.node.getComponent(cc.Animation).play('jump');
-            UIMgr.playAnimArr(this.spPlayer, ['jump', 'roll', 'down'], () => {
-                // this.spPlayer.node.scaleY = -this.spPlayer.node.scaleY;
-            });
-        }.bind(this));
+
+        cc.director.getPhysicsManager().enabled = true;
+        // cc.director.getPhysicsManager().debugDrawFlags = cc.PhysicsManager.DrawBits.e_aabbBit |
+        // cc.PhysicsManager.DrawBits.e_pairBit |
+        // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+        // cc.PhysicsManager.DrawBits.e_jointBit |
+        // cc.PhysicsManager.DrawBits.e_shapeBit
+        // ;
+        cc.director.getPhysicsManager().gravity = cc.v2(0, -320);
+
+        // this.touchLayer.on('touchstart', function (event) {
+        //     // this.spPlayer.node.getComponent(cc.Animation).play('jump');
+        //     UIMgr.playAnimArr(this.spPlayer, ['jump', 'roll', 'down'], () => {
+        //         // this.spPlayer.node.scaleY = -this.spPlayer.node.scaleY;
+        //     });
+        // }.bind(this));
+        // console.log(cc.find('Canvas/stageLayer/Block0'));
+        // console.log(cc.find('Canvas/stageLayer/Block1'));
+        // Util.readJsonData('data/json/stageData/Stage1', this._loadStage);
+        // let self = this;
+        Util.loadJsonFile('data/json/stageData/Stage1', this._initBlock, this);
+        
+ 
     },
 
     start() {
@@ -32,4 +53,27 @@ cc.Class({
     },
 
     // update (dt) {},
+    _initBlock(results){
+        this._stageData = results;
+        console.log(this._stageData);
+        this.blockLayer.destroyAllChildren();
+        let arr = this._stageData.blockList;
+        for (const item of arr) {
+            let name = BlockTemmplates.getBlockNameById(item.templateID);
+            let x = item.x;
+            let y = item.y;
+            let order = item.zOrder;
+            cc.loader.loadRes('blockPre/'+name, function(err, prefab){
+                if(err){
+                    console.log('init block err: ', err);
+                    return;
+                }
+                let blockNode = cc.instantiate(prefab);
+                console.log('this.blockLayer: ', this.blockLayer);
+                this.blockLayer.addChild(blockNode);
+                blockNode.position = cc.p(x, y);
+                blockNode.setLocalZOrder(order);
+            }.bind(this));
+        }
+    }
 });
