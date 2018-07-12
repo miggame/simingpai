eecdeb1c380a0f85d01cfa786dd5f1676427f74f
zhupengfei <1178227913@qq.com>
2018-07-12 18:35:12 +0800

    update physics json data

--- assets/test/Test.js
+++ assets/test/Test.js
@@ -12,6 +12,9 @@ cc.Class({
         _stageData: null,
         // _speed: null,
         blockLayer: { displayName: 'blockLayer', default: null, type: cc.Node },
+        pNode:cc.Node,
+        physicsPre: { displayName: 'physicsPre', default: null, type: cc.Prefab },
+        physicsLayer: { displayName: 'physicsLayer', default: null, type: cc.Node },
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -45,8 +48,11 @@ cc.Class({
         this.touchLayer.on('touchstart', this._changeGravity, this);
 
         Util.loadJsonFile('data/json/stageData/Stage2', this._initBlock, this);
+        Util.loadJsonFile('data/json/stageShapeData/stageShape1', this._initPhysics, this);
         this.spPlayer.node.position = cc.p(50, 320);
         // this._speed = GameData.runSpeed['RunningMan'];
+        console.log(this.pNode.getComponent(cc.PhysicsPolygonCollider));
+        
     },
 
     start() {
@@ -87,5 +93,31 @@ cc.Class({
         console.log('changeGravity');
         GameData.gravity.y = -GameData.gravity.y;
         cc.director.getPhysicsManager().gravity = GameData.gravity;
+    },
+
+    _initPhysics(results){
+        // console.log('result: ', results);
+        let totalArr = [];
+        for (const key in results) {
+            if (results.hasOwnProperty(key)) {
+                const element = results[key];
+                console.log('element: ', element);
+                let len = element.length/2;
+                let arr = [];
+                for(let i = 0;i<len;++i){
+                    let x = element[2*i];
+                    let y = element[2*i+1];
+                    arr.push(cc.p(x, y));
+                }
+                // totalArr.push(arr);
+                this._createPhysicsNode(arr);
+            }
+        }
+    },
+
+    _createPhysicsNode(arr){
+        let physicsNode = cc.instantiate(this.physicsPre);
+        this.physicsLayer.addChild(physicsNode);
+        physicsNode.getComponent(cc.PhysicsPolygonCollider).points = arr;
     }
 });
